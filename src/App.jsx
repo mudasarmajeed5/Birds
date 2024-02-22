755d6e35455112f58cc91d442cf9c224fc121ab3
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2024-02-22 18:16:34 +0500

    added marquee tag, its obselete but I had no other choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from "./components/Navbar"
 import TextAnimation from "./components/TextAnimation"
 import './App.css'
@@ -32,27 +32,29 @@ function App() {
     setBtncolor(!Btncolor);
     console.log(Btncolor);
   }
-  
+
 
 
   return (
     <>
-     {showButton && <div className='fixed left-2 top-[50vh]'><button id="dark-mode" className={`p-2 rounded-full ${Btncolor ? "bg-white": "bg-black"}`} onClick={(e) => { ToggleDarkMode(e); btnClr()}}> <img src={SwitchTheme} alt="" /></button></div>}
+      {showButton && <div className='fixed z-[3] left-2 top-[50vh]'><button id="dark-mode" className={`p-2 rounded-full ${Btncolor ? "bg-white" : "bg-black"}`} onClick={(e) => { ToggleDarkMode(e); btnClr() }}> <img src={SwitchTheme} alt="" /></button></div>}
       <Navbar />
       <div className="FirstPage">
         <div className="img">
           <img className='absolute object-cover object-center top-0 z-[-1000]  h-[100%] w-full' src={Imgsrc} alt="" />
         </div>
-        <div className="text_anime flex justify-center items-center h-[80vh] flex-col">
+        <div className="text_anime flex justify-center items-center h-[80vh] flex-col relative">
           <TextAnimation />
           <div className="flex gap-2">
             <button className='text-white backdrop:blur-sm mt-20 bg-green-500 px-4 py-2 rounded-md hover:bg-green-700 animate-bounce '><a href="#">Become a Partner</a></button>
             <button className='text-white backdrop:blur-sm mt-20 bg-red-600 px-4 py-2 rounded-md hover:bg-red-700 animate-bounce '><a href="#explore">Explore</a></button>
           </div>
-      <div className='flex justify-center items-center text-xl md:text-2xl lets_talk gap-2 mt-5 font-sans'>Wanna get in Touch?<span><a href="">Lets Talk...</a></span></div>
+          <div className='flex justify-center items-center text-xl md:text-2xl lets_talk gap-2 mt-5 font-sans'>Wanna get in Touch?<span><a href="">Lets Talk...</a></span></div>
         </div>
+        <marquee behavior="" className="absolute bottom-0 text-5xl md:text-5xl lg:text-6xl 2xl:text-9xl font-mono scrollText p-2 md:p-1" direction="scroll">Marketing . Technical . Creative. Marketing . Technical . Creative</marquee>
+        
       </div>
-      <SecondPage/>
+      <SecondPage />
 
     </>
   )
