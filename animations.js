d7ceb5bab16cd390d1acee7f05af155129858844
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2024-02-26 13:36:30 +0500

    Parallel scrolling, Changed Card Borders, Added Animations on Scroll to the secondPage

--- animations.js
+++ animations.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-    let offset70 = {offset:"70%"}
+    let offset80 = {offset:"80%"}
     let offset50 = {offset:"50%"}
+    let offset90 = {offset:"90%"}
     $(".creating_experiences").waypoint(function(){
         $(".creating_experiences").addClass("animate__animated animate__fadeInDown");
-    },offset70);
+    },offset80);
     $(".digital_landscape").waypoint(function(){
         $(".digital_landscape").addClass("animate__animated animate__fadeInUp");
-    },offset70);
+    },offset80);
     $(".card1").waypoint(function(){
         $(".card1").addClass("animate__animated animate__fadeInLeft");
-    },offset70);
+    },offset80);
     $(".card3").waypoint(function(){
         $(".card3").addClass("animate__animated animate__fadeInRight");
-    },offset70);
+    },offset80);
     $(".card2").waypoint(function(){
         $(".card2").addClass("animate__animated animate__fadeInUp");
-    },offset50);
+    },offset90);
 });
--- src/components/ThirdPage.jsx
+++ src/components/ThirdPage.jsx
@@ -1,63 +1,87 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./ThirdPage.css"
 import LaptopImg from "../assets/ThirdPageImages/Laptop_Image.png"
 import TableImg from "../assets/ThirdPageImages/Article_2_Table.jpg"
 import EventImg from "../assets/ThirdPageImages/NightOfHope.jpg"
-const ThirdPage = () => {
 
+const ThirdPage = () => {    
+    const leftRef = useRef(null);
+    const rightRef = useRef(null);
 
+    const handleScroll = (e) => {
+        const { scrollTop, scrollHeight, clientHeight } = rightRef.current;
+        const isAtTop = scrollTop === 0;
+        const isAtBottom = scrollHeight - scrollTop === clientHeight;
+    
+        if (isAtTop && e.deltaY < 0) {
+            document.body.style.overflow = '';
+        } else if (isAtBottom && e.deltaY > 0) {
+            document.body.style.overflow = '';
+        } else {
+            e.preventDefault();
+            rightRef.current.scrollTop += e.deltaY;
+        }
+    };
+    
+    const handleMouseEnter = () => {
+        document.body.style.overflow = 'hidden'; // Disable scrolling on the body
+    };
+
+    const handleMouseLeave = () => {
+        document.body.style.overflow = ''; // Re-enable scrolling on the body
+    };
     return (
-        <div className='ThirdPage md:p-3 xl:p-5 2xl:p-10 relative'>
-            <div className="leftside flex flex-col gap-10 items-center justify-center p-5 ">
-                <div className="ourwork text-5xl underline underline-offset-4">Our Work</div>
-                <div className="case_studies">
-                    Case Studies, a selection of <span id='successful' >successful projects.</span>
+            <div className='ThirdPage md:p-3 xl:p-5 2xl:p-10 relative' onWheel={handleScroll} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <div className="leftside flex flex-col gap-10 items-center justify-center p-5 " ref={leftRef}>
+                    <div className="ourwork text-5xl underline underline-offset-4">Our Work</div>
+                    <div className="case_studies">
+                        Case Studies, a selection of <span id='successful' >successful projects.</span>
+                    </div>
+                </div>
+                <div className="rightside overflow-y-auto flex flex-col gap-2 p-2" ref={rightRef}>
+                    <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
+                        <img className='w-[40%] md:w-[30%] rounded-md' src={LaptopImg} alt="" />
+                        <div className="flex flex-col content md:h-[70%] h-[100%]">
+                            <div className="head">Creativity</div>
+                            <p>Unleash the Power of Creativity: Your Guide to Thriving with a Creative Agency</p>
+                            <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
+                        <div className="flex flex-col content md:h-[70%] h-[100%]">
+                            <div className="head">Unforgettable Experiences</div>
+                            <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
+                            <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
+                        </div>
+                        <img className='w-[40%] md:w-[30%] rounded-md' src={TableImg} alt="" />
+                    </div>
+                    <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
+                        <img className='w-[40%] md:w-[30%] rounded-md' src={EventImg} alt="" />
+                        <div className="flex flex-col content md:h-[70%] h-[100%]">
+                            <div className="head">Implementation</div>
+                            <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
+                            <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
+                        <div className="flex flex-col content md:h-[70%] h-[100%]">
+                            <div className="head">Testimonials</div>
+                            <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
+                            <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
+                        </div>
+                        <img className='w-[40%] md:w-[30%] rounded-md' src={EventImg} alt="" />
+                    </div>
+                    <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
+                        <img className='w-[40%] md:w-[30%] rounded-md' src={EventImg} alt="" />
+                        <div className="flex flex-col content md:h-[70%] h-[100%]">
+                            <div className="head">Key Takeaways</div>
+                            <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
+                            <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="rightside overflow-y-auto flex flex-col gap-2 p-2">
-                <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
-                    <img className='w-[40%] md:w-[30%] rounded-md' src={LaptopImg} alt="" />
-                    <div className="flex flex-col content md:h-[70%] h-[100%]">
-                        <div className="head">Creativity</div>
-                        <p>Unleash the Power of Creativity: Your Guide to Thriving with a Creative Agency</p>
-                        <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
-                    </div>
-                </div>
-                <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
-                    <div className="flex flex-col content md:h-[70%] h-[100%]">
-                        <div className="head">Unforgettable Experiences</div>
-                        <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
-                        <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
-                    </div>
-                    <img className='w-[40%] md:w-[30%] rounded-md' src={TableImg} alt="" />
-                </div>
-                <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
-                    <img className='w-[40%] md:w-[30%] rounded-md' src={EventImg} alt="" />
-                    <div className="flex flex-col content md:h-[70%] h-[100%]">
-                        <div className="head">Implementation</div>
-                        <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
-                        <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
-                    </div>
-                </div>
-                <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
-                    <div className="flex flex-col content md:h-[70%] h-[100%]">
-                        <div className="head">Testimonials</div>
-                        <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
-                        <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
-                    </div>
-                    <img className='w-[40%] md:w-[30%] rounded-md' src={EventImg} alt="" />
-                </div>
-                <div className="flex items-center justify-between mt-5 gap-3  md:gap-5">
-                    <img className='w-[40%] md:w-[30%] rounded-md' src={EventImg} alt="" />
-                    <div className="flex flex-col content md:h-[70%] h-[100%]">
-                        <div className="head">Key Takeaways</div>
-                        <p>Unlocking the Digital Marketing Treasure Trove: Strategies for Success</p>
-                        <div><button className='px-3 py-1 rounded-sm'>Read More</button></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+        )
+    }
 
-export default ThirdPage
+    export default ThirdPage
